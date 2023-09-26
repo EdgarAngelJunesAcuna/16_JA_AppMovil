4ae7a8c804cc2898cbeac11e6db7cb01b2d08e8c
Edgar Angel Raziel Junes Acuña <111781870+EdgarAngelJunesAcuna@users.noreply.github.com>
2023-09-25 19:51:45 -0500

    AppMovil

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world_2023/presentation/screen/basic_intro.dart'; 
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BasicIntro(),
+      );
+  }
+}
+
+      /*Scaffold(
+        body: Center(child: Text('Hola Valle Grande...'))*/
